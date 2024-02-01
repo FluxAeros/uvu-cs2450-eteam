39907130db9df89663c5391aa78fd27240586fcd
Sean <seanedelen@hotmail.com>
2024-01-31 23:02:58 -0700

    Completed the tests

--- Control.py
+++ Control.py
@@ -3,22 +3,28 @@ from Memory import Memory
 class Control:
     @staticmethod
     def Branch(memory, index):
-        if (index >= 0 and index <= len(memory.mainMemory)):
+        if (index >= 0 and index <= (len(memory.mainMemory)-1)):
             return index
         else:
             raise IndexError('The memory location does not exist.')
 
     @staticmethod
     def BranchNeg(memory, index):
-        if (index >= 0 and index <= len(memory.mainMemory) and memory.accumulator < 0):
-            return index
+        if (index >= 0 and index <= (len(memory.mainMemory)-1)):
+            if (memory.accumulator < 0):
+                return index
+            else:
+                raise Exception('The accumulator is not negative.')
         else:
             raise IndexError('The memory location does not exist.')
 
     @staticmethod
     def BranchZero(memory, index):
-        if (index >= 0 and index <= len(memory.mainMemory) and memory.accumulator == 0):
-            return index
+        if (index >= 0 and index <= (len(memory.mainMemory)-1)):
+            if (memory.accumulator == 0):
+                return index
+            else:
+                raise Exception('The accumulator is not zero.')
         else:
             raise IndexError('The memory location does not exist.')
 
--- ControlTests.py
+++ ControlTests.py
@@ -5,28 +5,70 @@ from LoadStore import LoadStore
 
 class TestControlMethods(unittest.TestCase):
     def test_branch(self):
-        pass
+        memory = Memory()
+        index = 12
+
+        returnedIndex = Control.Branch(memory, index)
+        
+        self.assertEqual(index, returnedIndex)
 
     def test_branchIndexError(self):
-        pass
+        memory = Memory()
+        index = 100
+        
+        with self.assertRaises(IndexError):
+            Control.Branch(memory, index)
 
     def test_branchNeg(self):
-        pass
+        memory = Memory()
+        index = 9
+        memory.accumulator = -1024
+
+        returnedIndex = Control.BranchNeg(memory, index)
+
+        self.assertEqual(index, returnedIndex)
 
     def test_branchNegPositiveAccumulator(self):
-        pass
+        memory = Memory()
+        index = 11
+        memory.accumulator = 389
+
+        with self.assertRaises(Exception):
+            Control.BranchNeg(memory, index)
 
     def test_branchNegIndexError(self):
-        pass
+        memory = Memory()
+        index = 100
+        memory.accumulator = -74
+
+        with self.assertRaises(IndexError):
+            Control.BranchNeg(memory, index)
 
     def test_branchZero(self):
-        pass
+        memory = Memory()
+        index = 4
+        
+        returnedIndex = Control.BranchZero(memory, index)
+
+        self.assertEqual(index, returnedIndex)
 
     def test_branchZeroNonzeroAccumulator(self):
-        pass
+        memory = Memory()
+        index = 15
+        memory.accumulator = 500
+
+        with self.assertRaises(Exception):
+            Control.BranchZero(memory, index)
 
     def test_branchZeroIndexError(self):
-        pass
+        memory = Memory()
+        index = 100
+        
+        with self.assertRaises(IndexError):
+            Control.BranchZero(memory, index)
 
     def test_halt(self):
         pass
+
+if __name__ == '__main__':
+    unittest.main()

5e49177e01c5936a7e4909a5e2832d79b20c1f8f
Mason Fernandez <masonfernandez@outlook.com>
2024-02-08 01:30:52 -0700

    updated for getter setter and negatives

--- IO.py
+++ IO.py
@@ -2,24 +2,19 @@ from Memory import Memory
 
 class IO:
     @staticmethod
-    def Read(memory, index):
-        if not(index >= 0 and index <= (len(memory.mainMemory)-1)):
-            raise IndexError('Invalid memory location')
-        
-        in_num = input("Enter number in format 0000:\n")
+    def Read(memory: Memory(), index: int):
+        in_num = input("Enter number in format +/-0000:\n")
         try:
             in_num = int(in_num)
+            print(in_num)
         except:
             raise SyntaxError("input must be a number")
-        if (in_num < 0 or in_num > 9999):
+        if (in_num < -9999 or in_num > 9999):
             raise OverflowError("number is too large")
         
         memory.mainMemory[index] = in_num
         
 
     @staticmethod
-    def Write(memory, index):
-        if (index >= 0 and index <= (len(memory.mainMemory)-1)):
-            print(str(memory.mainMemory[index]).zfill(4))
-        else:
-            raise IndexError('Invalid memory location')
+    def Write(memory: Memory(), index: int):
+        print(str(memory.mainMemory[index]).zfill(4))

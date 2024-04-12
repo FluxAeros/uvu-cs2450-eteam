5cc4f29e1638145693a4ef22567e87289d3da909
Driedtoothpaste <149534920+Driedtoothpaste@users.noreply.github.com>
2024-04-11 19:26:07 -0600

    Create new window (#39)
    * Add files via upload
    
    * Added logic to check for old file and convert (#40)
    
    * Added logic to check for old file and convert
    
    * fix: made memory class variable
    
    ---------
    
    Co-authored-by: Mason Desktop <masonfernandez@outlook.com>
    
    * created run file view and toggle switch
    
    * Add files via upload
    
    * created run file view and toggle switch
    
    * simplified gui, fixed color changing
    
    * weird duplicate fix
    
    ---------
    
    Co-authored-by: Sean Edelen <seanedelen@hotmail.com>
    Co-authored-by: Mason Desktop <masonfernandez@outlook.com>

--- gui.py
+++ gui.py
@@ -1,3 +1,9 @@
+#gui will be updated to only perform its core responsibilities
+#gui home is responsible for selecting a valid file and changing colors now, everything else goes
+
+#gui will be updated to only perform its core responsibilities
+#gui home is responsible for selecting a valid file and changing colors now, everything else goes
+
 import re
 import threading
 import tkinter as tk
@@ -9,254 +15,109 @@ from processor import Processor
 from read_file import ReadFile
 from input_output import IO
 from read_file import ReadFile
-from gui_config import read_config, save_config
+from color_config import read_config, save_config
+from gui_run_file import RunView
+
 class GUI:
     from gui_view_file import view_file
 
     def __init__(self):
 
-        self.memory = Memory()
-
+        #initialize color scheme
+        saved_colors = read_config()
+        self.primary_color = saved_colors[0]
+        self.secondary_color = saved_colors[1]
         self.primary_color_widgets = []
         self.secondary_color_widgets = []
-        self.primary_color = '#4C721D'  # Dark green
-        self.off_color = '#FFFFFF'  # White
-
-        self.file_path = ''
-        self.trimmed_name = ""
+        
+        #class variables
         self.run_status = 0
+        self.file_path = ''
 
+        #create tkinter root window
         self.root = tk.Tk()
         self.root.geometry("800x500")
         self.root.minsize(500,400)
         self.root.title("UVSim Team E")
         self.root.configure(bg=self.primary_color)
+        self.primary_color_widgets.append(self.root)
 
-        # Initialize UI frames
-        self.init_status()
-        self.init_output()
-        self.init_input()
-        self.init_color_change()
-
+        # Initialize UI frame
+        self.init_select_file()
+        self.init_color_menu()
         self.root.mainloop()
-        
-    def get_input(self, event = 1):
-        GUI.user_input = self.input_text.get('1.0', tk.END).strip()  # Capture input
-        self.input_text.delete('0.0', tk.END)  # Clear the input field
-        self.display_output(GUI.user_input)
-        IO.input_ready_event.set()
-        return "break"
 
     def get_file(self, new_path = False):
         if new_path == False:
-            self.file_path = filedialog.askopenfilename(title="Select a file", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+            file_path = filedialog.askopenfilename(title="Select a file", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
         else:
-            self.file_path = new_path
-        if self.file_path != '':
-            self.trimmed_name = (re.search("([^\\/]+)$", self.file_path)).group()
-            self.file_name_label.config(text = f'Selected file: {self.trimmed_name}')
-            ReadFile.read_file_to_memory(self.memory, self.file_path)
-            self.display_output(f"Successfully loaded '{self.trimmed_name}'")
-            self.view_file_button.config(state = 'normal')
+            file_path = new_path
+        if file_path != '':
+            trimmed_name = (re.search("([^\\/]+)$", file_path)).group()
+            file_name = f'Selected file: {trimmed_name}'
+            new_file_view = RunView(self, file_path)
+
         else:
-            self.trimmed_name = 'NO FILE'
+            trimmed_name = 'NO FILE'
             self.file_name_label.config(text = f'Select a file to start')
             self.display_error("no file selected")
             self.view_file_button.config(state = 'disabled')
 
-    def toggle_run(self, errors = False):
-        if self.run_status == 0:
-            self.display_output(f"Running file {self.trimmed_name}")
-            self.run_status = 1
-            self.run_button.config(state=tk.DISABLED)
-        elif(self.run_status == 1 and not(errors)):
-            self.display_output(f"Finished running {self.trimmed_name}")
-            self.run_status = 0
-            self.run_button.config(state=tk.NORMAL)
-        else:
-            self.display_output(f"Terminated {self.trimmed_name} with errors")
-            self.run_status = 0
-            self.run_button.config(state=tk.NORMAL)
-        
-
-    def run_file(self):
-        self.toggle_run()
-        def process_file():
-            try:
-                Processor.process(self.memory, self)
-            except AttributeError:
-                self.display_error("no file selected")
-                self.toggle_run(True)
-                raise AttributeError("no file selected")
-            except FileNotFoundError:
-                self.display_error("no file selected")
-                self.toggle_run(True)
-                raise FileNotFoundError("no file selected")
-            except IndexError:
-                self.display_error("Invalid memory location")
-                self.toggle_run(True)
-                raise IndexError("Invalid memory location")
-            except ValueError:
-                self.display_error("Invalid file")
-                self.toggle_run(True)
-                raise ValueError
-            except:
-                self.display_error("unknown")
-                self.toggle_run(True)
-                raise RuntimeError("unknown")
-
-        processing_thread = threading.Thread(target=process_file)
-        processing_thread.start()
-
-    def init_status(self):
-        self.status_frame = tk.Frame(self.root, bg=self.primary_color)
-        self.primary_color_widgets.append(self.status_frame)
-        self.status_frame.pack(fill='x', padx=10, pady=10)
-        self.status_frame.columnconfigure(0, weight=1)
-        self.status_frame.columnconfigure(1, weight=6)
-        self.status_frame.columnconfigure(2, weight=1)
-
-        self.open_file_button = tk.Button(self.status_frame, text="Select file", font=('Arial', 18),
-                                          command=self.get_file, background="gray70")
-        self.open_file_button.grid(row=0, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
-
-        self.view_file_button = tk.Button(self.status_frame, text="View file", font=('Arial', 18),
-                                          command=self.view_file, bg="white", fg="black",state='disabled')
-        self.view_file_button.grid(row=0, column=2, sticky=tk.W+tk.E, padx=5, pady=5)
-
-        self.file_name_label = tk.Label(self.status_frame, text='Select a file to start', font=('Arial', 18), wraplength=400, bg="white",
+    def init_select_file(self):
+        self.status_frame = tk.Frame(self.root, bg='white')
+        self.select_file_label = tk.Label(self.status_frame, text='Select a file to start', font=('Arial', 18), wraplength=400, bg="white",
                                          fg="black")
-        self.file_name_label.grid(row=0, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
-
-        self.run_button = tk.Button(self.status_frame, text="Run", font=('Arial', 18), command=self.run_file,
-                                     bg="forestgreen", foreground='white')
-        self.run_button.grid(row=0, column=3, sticky=tk.W+tk.E, padx=5, pady=5)
-
+        
+        self.select_file_button = tk.Button(self.status_frame, text="Select file", font=('Arial', 18),
+                                          command=self.get_file, background=self.secondary_color)
+        self.secondary_color_widgets.append(self.select_file_button)
+        
+        self.select_file_label.pack()
+        self.select_file_button.pack()
         self.status_frame.pack(fill='x', padx=10, pady=10)
 
-    def init_output(self):
-        def on_mousewheel(event):
-            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
-        
-        frame = tk.Frame(self.root, width=300, height=200, bg="white")
-        frame.pack(expand=True, fill=tk.BOTH, padx=10)
-        self.secondary_color_widgets.append(frame)
-        
-        self.canvas = tk.Canvas(frame, bg="black", width=300, height=200, highlightthickness=0)
-        self.canvas.bind_all("<MouseWheel>", on_mousewheel)
-        
-        vbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=self.canvas.yview, elementborderwidth=0)
-        vbar.pack(side=tk.RIGHT, fill=tk.Y, padx=5, pady=5)
-        self.canvas.config(yscrollcommand=vbar.set)
-        
-        self.canvas.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=5, pady=5)
+    def init_color_menu(self):
 
-        self.curr_line = 1
-        self.o_font_size = 15
-        self.labels = []
-        self.display_output("Please select a file")
+        self.color_menu_frame = tk.Frame(self.root, bg='white')
+        self.color_menu_frame.columnconfigure(0, weight=1)
+        self.color_menu_frame.columnconfigure(1, weight=1)
 
-    def display_output(self, txt = ''):
-        label = tk.Label(self.canvas, text=txt, font=('Arial', self.o_font_size), fg="gray85", bg="black",
-                          anchor=tk.W, padx=5, pady=5)
-        self.labels.append(label)
-        self.canvas.create_window(0, self.curr_line * (self.o_font_size * 2), anchor=tk.NW, window=label)
-        self.curr_line += 1
-        
-        # Update the scroll region and canvas height
-        self.canvas.config(scrollregion=self.canvas.bbox("all"))
-        self.canvas.yview_moveto(1)
-        
-
-    def display_error(self, error_msg):
-        error_txt = 'ERROR: ' + error_msg
-        self.display_output(error_txt)
-
-    def init_input(self):
-        self.input_frame = tk.Frame(self.root, bg="white")
-        self.input_frame.columnconfigure(0, weight=1)
-        self.input_frame.columnconfigure(0, weight=1)
-        self.secondary_color_widgets.append(self.input_frame)
-
-        self.input_text = tk.Text(self.input_frame, height='2',font=('Arial', 16), background="gray70")
-        self.input_text.grid(row=0, column=0, padx=5, pady=5)
-        self.input_text.bind("<Return>", self.get_input)
-
-        self.input_button = tk.Button(self.input_frame, text="Enter", font=('Arial', 18),
-                                      command=self.get_input, background="gray70")
-        self.input_button.grid(row=0, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
-
-        self.input_frame.pack(fill='x', padx=10, pady=10)
-
-
-    main_color_items = []
-    secondary_color_items = []
-    def init_color_change(self):
-        self.change_main_color_button = tk.Button(self.status_frame, text="Change main color", font=('Arial'),
+        self.dark_mode_button = tk.Button(self.color_menu_frame, text="Dark Mode", font=('Arial'),
+                                                  command=self.toggle_dark_mode, background="gray70")
+        self.reset_colors_button = tk.Button(self.color_menu_frame, text="Reset", font=('Arial'),
+                                                  command=self.reset_colors, background="gray70")
+        self.change_main_color_button = tk.Button(self.color_menu_frame, text="Change main color", font=('Arial'),
                                                   command=self.change_main_color, background="gray70")
-        self.change_main_color_button.grid(row=1, column=0, sticky=tk.W + tk.E, padx=5, pady=5)
-
-        self.change_secondary_color_button = tk.Button(self.status_frame, text="Change secondary color", font=('Arial'),
+        self.change_secondary_color_button = tk.Button(self.color_menu_frame, text="Change secondary color", font=('Arial'),
                                                        command=self.change_secondary_color, background="gray70")
-        self.change_secondary_color_button.grid(row=1, column=2, sticky=tk.W + tk.E, padx=5, pady=5)
+        
+        self.dark_mode_button.grid(row=0, column=0, sticky=tk.W + tk.E, padx=5, pady=5)
+        self.reset_colors_button.grid(row=0, column=1, sticky=tk.W + tk.E, padx=5, pady=5)
+        self.change_main_color_button.grid(row=1, column=0, sticky=tk.W + tk.E, padx=5, pady=5)
+        self.change_secondary_color_button.grid(row=1, column=1, sticky=tk.W + tk.E, padx=5, pady=5)
 
-        self.main_color_items.append(self.change_main_color_button)
-        self.secondary_color_items.append(self.change_secondary_color_button)
+        self.color_menu_frame.pack()
 
+    def toggle_dark_mode(self):
+        pass
+    
+    def reset_colors(self):
+        pass
+    
     def change_main_color(self):
         color_info = colorchooser.askcolor(title="Choose color")
         if color_info[1]:
             self.primary_color = color_info[1]
             for widget in self.primary_color_widgets:
                 widget.configure(bg=self.primary_color)
-            save_config(self.primary_color, self.off_color)
+            save_config(self.primary_color, self.secondary_color)
             self.root.update_idletasks()
 
     def change_secondary_color(self):
         color_info = colorchooser.askcolor(title="Choose Button Color")
         if color_info[1]:
-            self.off_color = color_info[1]
+            self.secondary_color = color_info[1]
             for widget in self.secondary_color_widgets:
-                widget.configure(bg=self.off_color)
-            save_config(self.primary_color, self.off_color)
+                widget.configure(bg=self.secondary_color)
+            save_config(self.primary_color, self.secondary_color)
             self.root.update_idletasks()
-
-    def change_color(self, target='primary'):
-        color_info = colorchooser.askcolor(title="Choose color")
-        if color_info[1] is not None:
-            color_code = color_info[1]
-            if target == 'primary':
-                self.primary_color = color_code
-                for widget in self.primary_color_widgets:
-                    widget.configure(bg=color_code)
-            else:
-                self.off_color = color_code
-                for widget in self.secondary_color_widgets:
-                    widget.configure(bg=color_code)
-
-            self.apply_color_scheme()
-            save_config(self.primary_color, self.off_color)
-        else:
-            print("No color selected. No changes made.")
-
-    def apply_color_scheme(self):
-        self.root.configure(bg=self.primary_color)
-        for widget in self.primary_color_widgets:
-            widget.configure(bg=self.primary_color)
-            if isinstance(widget, (tk.Label, tk.Button, tk.Entry)):
-                widget.configure(fg=self.off_color)
-
-        for widget in self.secondary_color_widgets:
-            widget.configure(bg=self.off_color)
-            if isinstance(widget, (tk.Label, tk.Button, tk.Entry)):
-                widget.configure(fg=self.primary_color)
-
-        self.root.update_idletasks()
-
-    def init_color_change(self):
-        self.change_primary_color_button = tk.Button(self.status_frame, text="Change Primary Color",
-                                                     command=lambda: self.change_color('primary'), background="gray70")
-        self.change_primary_color_button.grid(row=1, column=0, sticky=tk.W + tk.E, padx=5, pady=5)
-        self.change_off_color_button = tk.Button(self.status_frame, text="Change Off-Color",
-                                                 command=lambda: self.change_color('off'), background="gray70")
-        self.change_off_color_button.grid(row=1, column=2, sticky=tk.W + tk.E, padx=5, pady=5)
---
+++ gui_run_file.py
@@ -0,0 +1,193 @@
+#this contains the new window for the gui file view
+#it will allow for the user to toggle between edit and run modes in the same contiguous window
+#design spec in my notes
+import re
+import threading
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import colorchooser
+from memory import Memory
+from processor import Processor
+from read_file import ReadFile
+from input_output import IO
+from read_file import ReadFile
+from color_config import read_config, save_config
+
+class RunView:
+
+    def __init__(self, gui, file_path):
+        self.gui = gui
+        self.file_path = file_path
+        self.run_status = 0
+    
+        self.primary_color_widgets = []
+        self.secondary_color_widgets = []
+        self.primary_color = '#4C721D'  # Dark green
+        self.off_color = '#FFFFFF'  # White
+
+        self.file_window = tk.Toplevel(self.gui.root)
+        self.file_window.geometry("800x500")
+        self.init_run_frame()
+        self.init_view_frame()
+
+        self.show_run()
+
+    def init_run_frame(self):
+        # Create the run frame with a green background
+        self.run_frame = tk.Frame(self.file_window, bg="light green")
+        self.run_status_bar(self.run_frame)
+        self.output_console(self.run_frame)
+        self.init_input(self.run_frame)
+
+    def init_view_frame(self):
+        #create the view frame
+        self.view_frame = tk.Frame(self.file_window, bg="light blue")
+        self.view_to_run_button = tk.Button(self.view_frame, text="View Button", command=self.show_run)
+        self.view_to_run_button.pack()
+
+    def show_view(self):
+        self.view_frame.pack(expand=True, fill=tk.BOTH)
+        self.run_frame.pack_forget()
+
+    def show_run(self):
+        self.run_frame.pack(expand=True, fill=tk.BOTH)
+        self.view_frame.pack_forget()
+    
+    def close(self):
+        self.file_window.destroy()
+
+    #run frame section
+    def run_status_bar(self, target_window): 
+        trimmed_name = (re.search("([^\\/]+)$", self.file_path)).group()       
+        self.status_frame = tk.Frame(target_window, bg=self.primary_color)
+        self.primary_color_widgets.append(self.status_frame)
+
+        #use column structure for placing elements
+        self.status_frame.columnconfigure(0, weight=1)
+        self.status_frame.columnconfigure(1, weight=6)
+        self.status_frame.columnconfigure(2, weight=1)
+        self.status_frame.columnconfigure(3, weight=1)
+
+        self.home_button = tk.Button(self.status_frame, text="Close", font=('Arial', 18), 
+                                     command=self.close, bg='white', fg='black')
+        self.file_name_label = tk.Label(self.status_frame, text=trimmed_name, font=('Arial', 18), wraplength=400, bg="white",
+                                         fg="black")
+        self.view_file_button = tk.Button(self.status_frame, text="View file", font=('Arial', 18),
+                                          command=self.show_view, bg="white", fg="black")
+        self.run_button = tk.Button(self.status_frame, text="Run", font=('Arial', 18), command=self.run_file,
+                                     bg="purple", foreground='white')
+        
+        #place each element onto grid
+        self.home_button.grid(row=0, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
+        self.file_name_label.grid(row=0, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
+        self.view_file_button.grid(row=0, column=2, sticky=tk.W+tk.E, padx=5, pady=5)
+        self.run_button.grid(row=0, column=3, sticky=tk.W+tk.E, padx=5, pady=5)
+
+        self.status_frame.pack(fill='x', padx=10, pady=10)
+
+    def display_error(self, error_msg):
+        error_txt = 'ERROR: ' + error_msg
+        self.display_output(error_txt)
+
+    def display_output(self, txt = ''):
+        label = tk.Label(self.canvas, text=txt, font=('Arial', self.o_font_size), fg="gray85", bg="black",
+                          anchor=tk.W, padx=5, pady=5)
+        self.labels.append(label)
+        self.canvas.create_window(0, self.curr_line * (self.o_font_size * 2), anchor=tk.NW, window=label)
+        self.curr_line += 1
+        
+        # Update the scroll region and canvas height
+        self.canvas.config(scrollregion=self.canvas.bbox("all"))
+        self.canvas.yview_moveto(1)
+
+    def output_console(self, target_frame):
+        def on_mousewheel(event):
+            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+        
+        frame = tk.Frame(target_frame, width=300, height=200, bg="white")
+        frame.pack(expand=True, fill=tk.BOTH, padx=10)
+        self.secondary_color_widgets.append(frame)
+        
+        self.canvas = tk.Canvas(frame, bg="black", width=300, height=200, highlightthickness=0)
+        self.canvas.bind_all("<MouseWheel>", on_mousewheel)
+        
+        vbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=self.canvas.yview, elementborderwidth=0)
+        vbar.pack(side=tk.RIGHT, fill=tk.Y, padx=5, pady=5)
+        self.canvas.config(yscrollcommand=vbar.set)
+        
+        self.canvas.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=5, pady=5)
+
+        self.curr_line = 1
+        self.o_font_size = 15
+        self.labels = []
+        self.display_output("Please select a file")
+
+    def init_input(self, target_frame):
+        self.input_frame = tk.Frame(target_frame, bg="white")
+        self.input_frame.columnconfigure(0, weight=1)
+        self.input_frame.columnconfigure(0, weight=1)
+        self.secondary_color_widgets.append(self.input_frame)
+
+        self.input_text = tk.Text(self.input_frame, height='2',font=('Arial', 16), background="gray70")
+        self.input_text.grid(row=0, column=0, padx=5, pady=5)
+        self.input_text.bind("<Return>", self.get_input)
+
+        self.input_button = tk.Button(self.input_frame, text="Enter", font=('Arial', 18),
+                                      command=self.get_input, background="gray70")
+        self.input_button.grid(row=0, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
+
+        self.input_frame.pack(fill='x', padx=10, pady=10)
+    
+    def get_input(self, event = 1):
+        self.user_input = self.input_text.get('1.0', tk.END).strip()  # Capture input
+        self.input_text.delete('0.0', tk.END)  # Clear the input field
+        self.display_output(self.user_input)
+        IO.input_ready_event.set()
+        return "break"
+    
+    def toggle_run(self, errors = False):
+        trimmed_name = (re.search("([^\\/]+)$", self.file_path)).group()
+        if self.run_status == 0:
+            self.display_output(f"Running file {trimmed_name}")
+            self.run_status = 1
+            self.run_button.config(state=tk.DISABLED)
+        elif(self.run_status == 1 and not(errors)):
+            self.display_output(f"Finished running {trimmed_name}")
+            self.run_status = 0
+            self.run_button.config(state=tk.NORMAL)
+        else:
+            self.display_output(f"Terminated {trimmed_name} with errors")
+            self.run_status = 0
+            self.run_button.config(state=tk.NORMAL)
+            
+    def run_file(self):
+        self.toggle_run()
+        def process_file():
+            try:
+                memory = Memory()
+                ReadFile.read_file_to_memory(memory, self.file_path)
+                Processor.process(memory, self)
+            except AttributeError:
+                self.display_error("no file selected")
+                self.toggle_run(True)
+                raise AttributeError("no file selected")
+            except FileNotFoundError:
+                self.display_error("no file selected")
+                self.toggle_run(True)
+                raise FileNotFoundError("no file selected")
+            except IndexError:
+                self.display_error("Invalid memory location")
+                self.toggle_run(True)
+                raise IndexError("Invalid memory location")
+            except ValueError:
+                self.display_error("Invalid file")
+                self.toggle_run(True)
+                raise ValueError
+            except:
+                self.display_error("unknown")
+                self.toggle_run(True)
+                raise RuntimeError("unknown")
+
+        processing_thread = threading.Thread(target=process_file)
+        processing_thread.start()

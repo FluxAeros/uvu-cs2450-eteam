d87d11ae21f34348d35f1aa09a252a25ba073c3a
Mason Desktop <masonfernandez@outlook.com>
2024-04-12 23:34:58 -0600

    fixed file writing error, updated prompt

--- read_file.py
+++ read_file.py
@@ -13,11 +13,11 @@ class ReadFile:
 
                 for command in file:
                     if (len(re.sub("[^0-9]", "", command)) == 4 and checked_convert == False):
-                        convert = messagebox.askyesno("Old File Conversion", "This file is in the old format of four digit words. Would you like to convert to the new six digit format?")
+                        convert = messagebox.askyesno("Old File Conversion", "This file is in the old 4 digit format. Would you like to update and overwrite the file to 6 digits?")
                         checked_convert = True
                         if (convert):
                             numerical_command = re.sub("[^0-9]", "", command)
-                            print(numerical_command[:2])
+                            #print(numerical_command[:2])
                             sign = command[0]
                             if (numerical_command[:2] in ("10", "11", "20", "21", "30", "31", "32", "33", "40", "41", "42", "43")):
                                 numerical_command = "0" + numerical_command[:2] + "0" + numerical_command[2:]
@@ -30,7 +30,7 @@ class ReadFile:
                             index += 1
                     elif (len(re.sub("[^0-9]", "", command)) == 4 and convert == True):
                         numerical_command = re.sub("[^0-9]", "", command)
-                        print(numerical_command[:2])
+                        #print(numerical_command[:2])
                         sign = command[0]
                         if (numerical_command[:2] in ("10", "11", "20", "21", "30", "31", "32", "33", "40", "41", "42", "43")):
                             numerical_command = "0" + numerical_command[:2] + "0" + numerical_command[2:]
@@ -49,9 +49,13 @@ class ReadFile:
                     else:
                         raise ValueError("Invalid command")
                 if (convert):
-                    file.truncate(0)
-                    for command in converted_file:
-                        file.write("".join(command) + "\n")
+                    file.close()
+                    with open(file_path, 'w') as file:
+                        for i, command in enumerate(converted_file):
+                            if i < len(converted_file) - 1:
+                                file.write("".join(command) + "\n")
+                            else:
+                                file.write("".join(command))
         except FileNotFoundError:
             raise FileNotFoundError("File not found")
 
